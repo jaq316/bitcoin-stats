8db771253730c1420d1a63483b0a449c37c080da
jaq316 <jacques@jacquessnyman.co.za>
2013-11-13 13:43:27 +0200

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,13 @@
-require(function(require){
+require.config({
+    paths: {
+        text: 'durandal/amd/text',
+        'durandal/app' : '//cdn.jsdelivr.net/durandal/2.0.1/js/app.js',
+        'durandal/viewLocator' : '//cdn.jsdelivr.net/durandal/2.0.1/js/viewLocator.js',
+        'durandal/system' : '//cdn.jsdelivr.net/durandal/2.0.1/js/system.js',
+        'durandal/plugins/router' : '//cdn.jsdelivr.net/durandal/2.0.1/js/plugins/router.js'
+    }
+});
+
+define(['durandal/app', 'durandal/viewLocator', 'durandal/system', 'durandal/plugins/router'],
 
 });
