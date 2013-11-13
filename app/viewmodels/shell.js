93fb88802de838e1af2a1c5d64c8220edd00277c
jaq316 <jacques@jacquessnyman.co.za>
2013-11-13 14:17:26 +0200

    Create shell.js

---
+++ app/viewmodels/shell.js
@@ -0,0 +1,40 @@
+define(['durandal/system', 'durandal/plugins/router', 'services/logger', 'durandal/app', 'Q'], 
+function (system, router, logger, Q) {
+
+        //#region Initialization functions
+        var initializers = [];
+        //#endregion
+        
+        //#region Internal Methods
+
+        function activate() {
+            return Q.all(initializers).then(initRoutes)
+                .then(boot);
+
+        };
+        
+        function initRoutes() {
+            router.mapRoute('home', null, "Home", false);
+            log(app.title + ' Loaded!', null, false);
+            return true;
+        }
+        function boot() {
+            return router.activate('home');
+        };
+
+        function log(msg, data, showToast) {
+            logger.info(msg, data, system.getModuleId(shell));
+        };
+        
+        //#endregion
+        
+        //#region Public Properties
+        
+        var shell = {
+            activate: activate,
+            router: router
+        };
+
+        return shell;
+
+});
