0f72a68a26d579733e3e21350577213aa860b9a0
jaq316 <jacques@jacquessnyman.co.za>
2013-11-13 14:06:59 +0200

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,15 +1,40 @@
-require.config({
+requirejs.config({
     paths: {
-        text: 'durandal/amd/text',
-        'durandal/app' : '//cdn.jsdelivr.net/durandal/2.0.1/js/app.js',
-        'durandal/viewLocator' : '//cdn.jsdelivr.net/durandal/2.0.1/js/viewLocator.js',
-        'durandal/system' : '//cdn.jsdelivr.net/durandal/2.0.1/js/system.js',
-        'durandal/plugins/router' : '//cdn.jsdelivr.net/durandal/2.0.1/js/plugins/router.js'
+        'text': '//cdn.jsdelivr.net/requirejs/2.1.9/plugins/text',
+        'durandal':'//cdn.jsdelivr.net/durandal/2.0.1/js/',
+        'plugins' : '//cdn.jsdelivr.net/durandal/2.0.1/js/plugins/',
+        'transitions' : '//cdn.jsdelivr.net/durandal/2.0.1/js/transitions/',
+        'knockout': '//cdn.jsdelivr.net/knockout/3.0.0/knockout',
+        'bootstrap': '//cdn.jsdelivr.net/bootstrap/3.0.2/js/bootstrap',
+        'jquery': '//cdn.jsdelivr.net/jquery/2.0.3/jquery-2.0.3'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        }
     }
 });
 
-define(['durandal/app', 'durandal/viewLocator', 'durandal/system', 'durandal/plugins/router'],
-    function (app, viewLocator, system, router) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
 
-    }
-);
+    app.title = 'BitCoin Stats';
+
+    //specify which plugins to install and their configuration
+    app.configurePlugins({
+        router:true,
+        dialog: true
+    });
+
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application.
+        app.setRoot('shell');
+    });
+});
